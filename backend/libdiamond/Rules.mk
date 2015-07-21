90db216360555310e09ed265d2ec904040cd0d40
Irene Y Zhang <iyzhang@cs.washington.edu>
2015-07-21 16:02:17 -0700

    adding some more dependencies

--- backend/libdiamond/Rules.mk
+++ backend/libdiamond/Rules.mk
@@ -1,6 +1,6 @@
 d := $(dir $(lastword $(MAKEFILE_LIST)))
 
-$(d)libdiamond.so: $(patsubst %.o,%-pic.o, $(OBJS-client) $(OBJS-data-types))
+$(d)libdiamond.so: $(patsubst %.o,%-pic.o, $(LIB-message) $(OBJS-client) $(OBJS-data-types))
 LDFLAGS-$(d)libdiamond.so += -shared -lredox -lhiredis -lpython2.7 -lboost_python -Wl,-rpath=redox/release 
 
 BINS += $(d)libdiamond.so
