e4ec400e55b8e9e59ca10b370630cb5252e91941
Niel Lebeck <niel.lebeck@gmail.com>
2015-07-21 17:03:01 -0700

    More Mac OS X and Homebrew changes

--- backend/libdiamond/Rules.mk
+++ backend/libdiamond/Rules.mk
@@ -1,6 +1,11 @@
 d := $(dir $(lastword $(MAKEFILE_LIST)))
 
 $(d)libdiamond.so: $(patsubst %.o,%-pic.o, $(LIB-message) $(OBJS-client) $(OBJS-data-types))
-LDFLAGS-$(d)libdiamond.so += -shared -lredox -lhiredis -lpython2.7 -lboost_python -Wl,-rpath=redox/release 
+LDFLAGS-$(d)libdiamond.so += -shared -lredox -lhiredis -lpython2.7 -lboost_python
+
+UNAME_S := $(shell uname -s)
+ifneq ($(UNAME_S),Darwin)
+    LDFLAGS-$(d)libdiamond.so += -Wl,-rpath=redox/release
+endif
 
 BINS += $(d)libdiamond.so
