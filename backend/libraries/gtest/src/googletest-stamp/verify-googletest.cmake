aafa2d2fd5875a47bf8929d892e03f3d92472999
Irene Zhang <iyzhang@cs.washington.edu>
2015-07-28 14:43:29 -0700

    reorg

--- backend/libraries/gtest/src/googletest-stamp/verify-googletest.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/Users/irene/uw/diamond/src/backend/gtest/src/gtest-1.7.0.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
