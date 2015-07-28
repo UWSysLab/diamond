b60a8e7b3f02071237ebad176b29dda637b4908f
Irene Zhang <iyzhang@cs.washington.edu>
2015-07-28 15:14:43 -0700

    removing more dead files

--- backend/src/data_types/Rules.mk~
+++
@@ -1,5 +0,0 @@
-d := $(dir $(lastword $(MAKEFILE_LIST)))
-
-SRCS += $(addprefix $(d), client.cc)
-
-OBJS-client := $(o)client.o 
--- backend/src/lib/Rules.mk
+++
@@ -1,8 +0,0 @@
-d := $(dir $(lastword $(MAKEFILE_LIST)))
-
-SRCS += $(addprefix $(d), \
-	lookup3.cc message.cc)
-
-LIB-hash := $(o)lookup3.o
-
-LIB-message := $(o)message.o $(LIB-hash)
