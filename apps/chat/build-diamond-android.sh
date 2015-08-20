25646534f6c109cfb4d5d6ac67baf29e22e57492
Niel Lebeck <niel.lebeck@gmail.com>
2015-08-20 13:13:28 -0700

    Made Android build script more general

--- apps/chat/build-diamond-android.sh
+++ apps/chat/build-diamond-android.sh
@@ -1,13 +1,16 @@
-DIAMOND_SRC_DIR="/home/niel/systems/diamond-src"
+DIAMOND_SRC_DIR=$1
 DIAMOND_BACKEND_DIR="$DIAMOND_SRC_DIR/backend"
-PROJECT_DIR="$DIAMOND_SRC_DIR/apps/chat/DiMessage"
+PROJECT_DIR=$2
 
+# build ARM binaries
 cd $DIAMOND_BACKEND_DIR/build-arm
 make
 
+# build Java bindings
 cd $DIAMOND_BACKEND_DIR/src/bindings/java
 mvn package
 
+# copy libraries and JARs into the Android project
 cp $DIAMOND_BACKEND_DIR/build-arm/libdiamond.so $PROJECT_DIR/libs/armeabi
 cp $DIAMOND_BACKEND_DIR/build-arm/hiredis/src/libhiredis.so $PROJECT_DIR/libs/armeabi
 cp $DIAMOND_BACKEND_DIR/src/bindings/java/target/classes/arm-lib/libjniDiamond.so $PROJECT_DIR/libs/armeabi
