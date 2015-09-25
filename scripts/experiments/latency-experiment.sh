0d1e2f0b816b22e39b0ccba53a8cb681a3a0ebce
Niel Lebeck <niel.lebeck@gmail.com>
2015-09-25 16:18:21 -0700

    latency-experiment script back to normal

--- scripts/experiments/latency-experiment.sh
+++ scripts/experiments/latency-experiment.sh
@@ -8,8 +8,7 @@ classpath="$PROJECT_DIR/bin:$JAVA_BINDINGS_DIR/libs/javacpp.jar:$JAVA_BINDINGS_D
 nativePath="$JAVA_BINDINGS_DIR/target/classes/x86-lib:$DIAMOND_SRC/backend/build"
 
 export LD_LIBRARY_PATH=$nativePath
-#java -cp $classpath -Djava.library.path=$nativePath Main 0 client1 > client1.log 2> error1.log &
-#java -cp $classpath -Djava.library.path=$nativePath Main 1.0 client2 > client2.log 2> error2.log &
-java -cp $classpath -Djava.library.path=$nativePath Main 0 client1 > client1.log 2> error1.log
+java -cp $classpath -Djava.library.path=$nativePath Main 0 client1 > client1.log 2> error1.log &
+java -cp $classpath -Djava.library.path=$nativePath Main 1.0 client2 > client2.log 2> error2.log &
 sleep 1
-#system("./latency-measurements.pl");
+$DIAMOND_SRC/scripts/experiments/latency-measurements.pl
