7ea1bbecb2f5314a9b2421df89e85c6c29da73d9
Niel Lebeck <niel.lebeck@gmail.com>
2015-09-25 16:19:34 -0700

    Remove retry from chat benchmarking client read method

--- apps/chat/DesktopChat/src/Main.java
+++ apps/chat/DesktopChat/src/Main.java
@@ -48,12 +48,12 @@ public class Main {
 		while (committed == 0) {
 			readTimeStart = System.currentTimeMillis();
 			Diamond.DObject.TransactionBegin();
-			if (updateTime.Value() == lastReadUpdateTime) {
+			/*if (updateTime.Value() == lastReadUpdateTime) {
 				Diamond.DObject.TransactionRetry();
 				continue;
 			}
+			lastReadUpdateTime = updateTime.Value();*/
 			result = messageList.Members();
-			lastReadUpdateTime = updateTime.Value();
 			committed = Diamond.DObject.TransactionCommit();
 		}
 		readTimeEnd = System.currentTimeMillis();
