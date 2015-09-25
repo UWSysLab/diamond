6805faddd0059a8b70c22eabdc4a4f23b13f4518
Niel Lebeck <niel.lebeck@gmail.com>
2015-09-24 17:09:06 -0700

    Wrote initial experiment script

---
+++ scripts/experiments/run-experiment.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $DIAMOND_SRC = "/home/nl35/research/diamond-src";
+my $PROJECT_DIR = "$DIAMOND_SRC/apps/chat/DesktopChat";
+my $JAVA_BINDINGS_DIR = "$DIAMOND_SRC/backend/src/bindings/java";
+
+my $classpath = "$PROJECT_DIR/bin:$JAVA_BINDINGS_DIR/libs/javacpp.jar:$JAVA_BINDINGS_DIR/target/diamond-1.0-SNAPSHOT.jar";
+my $nativePath = "$JAVA_BINDINGS_DIR/target/classes/x86-lib:$DIAMOND_SRC/backend/build";
+
+$ENV{"LD_LIBRARY_PATH"} = $nativePath;
+system("java -cp $classpath -Djava.library.path=$nativePath Main");
