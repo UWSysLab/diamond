1098b3e69dac72c5d726c1fa8b74543a685b7e1e
Niel Lebeck <niel.lebeck@gmail.com>
2015-09-27 11:40:25 -0700

    Check to make sure paths are valid

--- scripts/experiments/latency-experiment.sh
+++ scripts/experiments/latency-experiment.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-DIAMOND_SRC="/home/nl35/research/diamond-src"
+DIAMOND_SRC="/home/niel/research/diamond-src"
 PROJECT_DIR="$DIAMOND_SRC/apps/chat/DesktopChat"
 JAVA_BINDINGS_DIR="$DIAMOND_SRC/backend/src/bindings/java"
+
+if [ ! -d $DIAMOND_SRC ]
+then
+    echo "Error: DIAMOND_SRC directory does not exist"
+    exit
+fi
+
+if [ ! -e "$PROJECT_DIR/bin/Main.class" ]
+then
+    echo "Error: DesktopChat has not been compiled"
+    exit
+fi
 
 classpath="$PROJECT_DIR/bin:$JAVA_BINDINGS_DIR/libs/javacpp.jar:$JAVA_BINDINGS_DIR/target/diamond-1.0-SNAPSHOT.jar"
 nativePath="$JAVA_BINDINGS_DIR/target/classes/x86-lib:$DIAMOND_SRC/backend/build"
