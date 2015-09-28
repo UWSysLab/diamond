7d24f4c4c1fc4f7913822baa2e154a94a37efffc
Niel Lebeck <niel.lebeck@gmail.com>
2015-09-28 13:03:32 -0700

    Client now computes average itself

--- scripts/experiments/latency-experiment.sh
+++ scripts/experiments/latency-experiment.sh
@@ -19,15 +19,12 @@ fi
 classpath="$PROJECT_DIR/bin:$JAVA_BINDINGS_DIR/libs/javacpp.jar:$JAVA_BINDINGS_DIR/target/diamond-1.0-SNAPSHOT.jar"
 nativePath="$JAVA_BINDINGS_DIR/target/classes/x86-lib:$DIAMOND_SRC/backend/build"
 
-rm client1.log
-rm client2.log
 rm error1.log
 rm error2.log
 
 export LD_LIBRARY_PATH=$nativePath
-java -cp $classpath -Djava.library.path=$nativePath Main fixed 1000 0 client1 latencyroom > client1.log 2> error1.log &
+java -cp $classpath -Djava.library.path=$nativePath Main fixed 1000 0 writer latencyroom 2> error1.log &
 PID1=$!
-java -cp $classpath -Djava.library.path=$nativePath Main fixed 1000 1.0 client2 latencyroom > client2.log 2> error2.log &
+java -cp $classpath -Djava.library.path=$nativePath Main fixed 1000 1.0 reader latencyroom 2> error2.log &
 PID2=$!
 wait $PID1 $PID2
-$DIAMOND_SRC/scripts/experiments/latency-measurements.pl
