f8cc3b6033bf6f57e59dc68c020347aad2c47678
Niel Lebeck <niel.lebeck@gmail.com>
2015-09-30 13:14:43 -0700

    Updated experiment scripts

--- scripts/experiments/latency-experiment.sh
+++ scripts/experiments/latency-experiment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-./desktop-chat-wrapper.sh fixed 1000 0 writer latencyroom 2> error1.log &
+#CONCURRENCY=transaction
+CONCURRENCY=atomic
+
+./desktop-chat-wrapper.sh fixed 1000 0 $CONCURRENCY writer latencyroom 2> error1.log &
 PID1=$!
-./desktop-chat-wrapper.sh fixed 1000 1.0 reader latencyroom 2> error2.log &
+./desktop-chat-wrapper.sh fixed 1000 1.0 $CONCURRENCY reader latencyroom 2> error2.log &
 PID2=$!
 wait $PID1 $PID2
