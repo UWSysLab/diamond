1f107dba138f4bc9612a20a5e3959d14b1c988b1
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-13 13:42:22 -0700

    Added awk code to isolate latency numbers

--- scripts/experiments/latency-experiment.sh
+++ scripts/experiments/latency-experiment.sh
@@ -3,18 +3,21 @@
 CONCURRENCY=transaction
 #CONCURRENCY=atomic
 
-SERVER=localhost
+SERVER=moranis.cs.washington.edu
 
-rm desktop-latency-error*
-
-./desktop-chat-wrapper.sh fixed 1000 0 transaction verbose $SERVER writer latencyroom > desktopchat-transaction-writer.txt 2> desktop-latency-error1.txt &
+./desktop-chat-wrapper.sh fixed 1000 0 transaction verbose $SERVER writer latencyroom > desktopchat-transaction-writer-log.txt 2> desktop-latency-error1.txt &
 PID1=$!
-./desktop-chat-wrapper.sh fixed 1000 1.0 transaction verbose $SERVER reader latencyroom > desktopchat-transaction-reader.txt 2> desktop-latency-error2.txt &
+./desktop-chat-wrapper.sh fixed 1000 1.0 transaction verbose $SERVER reader latencyroom > desktopchat-transaction-reader-log.txt 2> desktop-latency-error2.txt &
 PID2=$!
 wait $PID1 $PID2
 
-./desktop-chat-wrapper.sh fixed 1000 0 atomic verbose $SERVER writer latencyroom > desktopchat-atomic-writer.txt 2> desktop-latency-error3.txt &
+./desktop-chat-wrapper.sh fixed 1000 0 atomic verbose $SERVER writer latencyroom > desktopchat-atomic-writer-log.txt 2> desktop-latency-error3.txt &
 PID1=$!
-./desktop-chat-wrapper.sh fixed 1000 1.0 atomic verbose $SERVER reader latencyroom > desktopchat-atomic-reader.txt 2> desktop-latency-error4.txt &
+./desktop-chat-wrapper.sh fixed 1000 1.0 atomic verbose $SERVER reader latencyroom > desktopchat-atomic-reader-log.txt 2> desktop-latency-error4.txt &
 PID2=$!
 wait $PID1 $PID2
+
+cat desktopchat-transaction-writer-log.txt | awk '$1 !~ /Summary/ { print $4 }' > desktopchat-transaction-writer-latencies.txt
+cat desktopchat-transaction-reader-log.txt | awk '$1 !~ /Summary/ { print $4 }' > desktopchat-transaction-reader-latencies.txt
+cat desktopchat-atomic-writer-log.txt | awk '$1 !~ /Summary/ { print $4 }' > desktopchat-atomic-writer-latencies.txt
+cat desktopchat-atomic-reader-log.txt | awk '$1 !~ /Summary/ { print $4 }' > desktopchat-atomic-reader-latencies.txt
