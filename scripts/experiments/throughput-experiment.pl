ed55c4905b714bb0e72dd221c3c85c419e43cb26
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-14 09:38:41 -0700

    Added experiment parsing code to throughput experiment script

--- scripts/experiments/throughput-experiment.pl
+++ scripts/experiments/throughput-experiment.pl
@@ -6,48 +6,85 @@ use strict;
 my $time = 5;
 my $readFraction = 0.9;
 my $prefix = "throughputlog";
-my $errPrefix = "throughputerror";
-my $concurrency = "transaction";
+my $errPrefix = "throughput";
+
 my $server = "localhost";
 
-for (my $numClients = 1; $numClients < 20; $numClients++) {
-    system("rm $prefix.*");
-    system("rm $errPrefix.*");
+my $concurrency = "transaction";
+my $log = "desktopchat-throughput-transaction-log.txt";
+my $throughputFile = "desktopchat-throughput-transaction.txt";
+my $abortRateFile = "desktopchat-throughput-transaction-abortrates.txt";
 
-    for (my $i = 0; $i < $numClients; $i++) {
-        system("./desktop-chat-wrapper.sh timed $time $readFraction $concurrency concise $server client$i throughputroom > $prefix.$i.txt 2> $errPrefix.$i.error &");
-    }
+doExperiment();
+parseThroughputs();
+parseAbortRates();
 
-    sleep($time + 5);
+$concurrency = "atomic";
+$log = "desktopchat-throughput-atomic-log.txt";
+$throughputFile = "desktopchat-throughput-atomic.txt";
 
-    my $totalNumActions = 0;
-    my $abortRateSum = 0;
+doExperiment();
+parseThroughputs();
 
-    for (my $i = 0; $i < $numClients; $i++) {
-        my $lines = 0;
-        open(LOG, "$prefix.$i.txt");
-        while(<LOG>) {
-            chomp;
-            my @lineSplit = split(/\s+/);
-            my $numActions = $lineSplit[3];
-            $totalNumActions += $numActions;
-            if ($concurrency eq "transaction") {
-                my $abortRate = $lineSplit[6];
-                $abortRateSum += $abortRate;
+sub parseThroughputs {
+    system("cat $log | awk '
+        BEGIN { print \"clients\tthroughput\" }
+        { print \$3, \"\t\", \$5 }
+        ' > $throughputFile");
+}
+
+sub parseAbortRates {
+    system("cat $log | awk '
+        BEGIN { print \"clients\tabortrate\" }
+        { print \$3, \"\t\", \$11 }
+        ' > $abortRateFile");
+}
+
+
+sub doExperiment {
+    open(FILE, "> $log");
+    for (my $numClients = 1; $numClients < 20; $numClients++) {
+        print("Experiment: $log Clients: $numClients\n");
+
+        system("rm $prefix.*");
+        system("rm $errPrefix.*");
+
+        for (my $i = 0; $i < $numClients; $i++) {
+            system("./desktop-chat-wrapper.sh timed $time $readFraction $concurrency concise $server client$i throughputroom > $prefix.$i.txt 2> $errPrefix.$i.error &");
+        }
+
+        sleep($time + 5);
+
+        my $totalNumActions = 0;
+        my $abortRateSum = 0;
+
+        for (my $i = 0; $i < $numClients; $i++) {
+            my $lines = 0;
+            open(LOG, "$prefix.$i.txt");
+            while(<LOG>) {
+                chomp;
+                my @lineSplit = split(/\s+/);
+                my $numActions = $lineSplit[3];
+                $totalNumActions += $numActions;
+                if ($concurrency eq "transaction") {
+                    my $abortRate = $lineSplit[6];
+                    $abortRateSum += $abortRate;
+                }
+                $lines = $lines + 1;
             }
-            $lines = $lines + 1;
+            close(LOG);
+            if ($lines != 1) {
+                die "Error: log file $i has $lines lines";
+            }
         }
-        close(LOG);
-        if ($lines != 1) {
-            die "Error: log file $i has $lines lines";
-        }
-    }
 
-    my $throughput = $totalNumActions / $time;
-    print("Num clients: $numClients\tThroughput: $throughput\tConcurrency: $concurrency");
-    if ($concurrency eq "transaction") {
-        my $avgAbortRate = $abortRateSum / $numClients;
-        print("\tAvg abort rate: $avgAbortRate");
+        my $throughput = $totalNumActions / $time;
+        print(FILE "Num clients: $numClients\tThroughput: $throughput\tConcurrency: $concurrency");
+        if ($concurrency eq "transaction") {
+            my $avgAbortRate = $abortRateSum / $numClients;
+            print(FILE "\tAvg abort rate: $avgAbortRate");
+        }
+        print(FILE "\n");
     }
-    print("\n");
+    close(FILE);
 }
