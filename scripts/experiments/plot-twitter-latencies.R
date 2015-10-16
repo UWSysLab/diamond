c1137ef36f012f3966e64ce70dab14b75392fac5
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-16 15:00:10 -0700

    Updated graph scripts

--- scripts/experiments/plot-twitter-latencies.R
+++ scripts/experiments/plot-twitter-latencies.R
@@ -10,12 +10,13 @@ diamond$system = "diamond"
 data <- rbind(original, diamond)
 data <- rename(data, c("V1" = "latency"))
 
-pdf("twitter-latency-plots.pdf")
+pdf("android-twitter-latency-plots.pdf")
 
-ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf()
+ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf() + coord_cartesian(xlim = c(0, 300))
 ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf() + coord_cartesian(xlim = c(0, 50))
+ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf()
 
-ggplot(data, aes(x=factor(system), y=latency)) + geom_boxplot()
 ggplot(data, aes(x=factor(system), y=latency)) + geom_boxplot() + coord_cartesian(ylim = c(0, 50))
+ggplot(data, aes(x=factor(system), y=latency)) + geom_boxplot()
 
 dev.off()
