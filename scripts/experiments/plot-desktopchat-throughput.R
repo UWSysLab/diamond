2e46654763260a701e4c27fd53024ae007ca7741
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-16 16:01:47 -0700

    Plot stale throughput results

--- scripts/experiments/plot-desktopchat-throughput.R
+++ scripts/experiments/plot-desktopchat-throughput.R
@@ -1,12 +1,22 @@
 library(ggplot2)
 library(plyr)
 
-data <- read.table("desktopchat-throughput/throughput-results.txt", header = TRUE)
-abortRates <- read.table("desktopchat-throughput/abortrate-results.txt", header = TRUE)
+nostaleData <- read.table("desktopchat-throughput/nostale-results.txt", header = TRUE)
+nostaleAbortRates <- read.table("desktopchat-throughput/nostale-abortrate.txt", header = TRUE)
+staleData <- read.table("desktopchat-throughput/stale-results.txt", header = TRUE)
+staleAbortRates <- read.table("desktopchat-throughput/stale-abortrate.txt", header = TRUE)
+
+nostaleData$staleness = "no stale"
+nostaleAbortRates$staleness = "no stale"
+staleData$staleness = "stale"
+staleAbortRates$staleness = "stale"
+
+throughputs = rbind(nostaleData, staleData)
+abortRates = rbind(nostaleAbortRates, staleAbortRates)
 
 pdf("desktop-chat-throughput-plots.pdf")
 
-ggplot(data, aes(x=clients, y=throughput)) + geom_line()
-ggplot(abortRates, aes(x=clients, y=abortrate)) + geom_line()
+ggplot(throughputs, aes(x=clients, y=throughput, color=staleness)) + geom_line()
+ggplot(abortRates, aes(x=clients, y=abortrate, color=staleness)) + geom_line()
 
 dev.off()
