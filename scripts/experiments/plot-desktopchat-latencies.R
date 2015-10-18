997f1ae9a673ace40fb0409a8481e4630a5d820b
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-18 16:14:12 -0700

    Added baseline chat client to latency graph

--- scripts/experiments/plot-desktopchat-latencies.R
+++ scripts/experiments/plot-desktopchat-latencies.R
@@ -5,13 +5,17 @@ atomicWriter <- read.table("desktopchat-latency/atomic-writer-latencies.txt")
 atomicReader <- read.table("desktopchat-latency/atomic-reader-latencies.txt")
 transactionWriter <- read.table("desktopchat-latency/transaction-writer-latencies.txt")
 transactionReader <- read.table("desktopchat-latency/transaction-reader-latencies.txt")
+baselineWriter <- read.table("desktopchat-latency/baseline-writer-latencies.txt")
+baselineReader <- read.table("desktopchat-latency/baseline-reader-latencies.txt")
 
 atomicWriter$client = "AtomicWriter";
 atomicReader$client = "AtomicReader";
 transactionWriter$client = "TransactionWriter";
 transactionReader$client = "TransactionReader";
+baselineWriter$client = "BaselineWriter";
+baselineReader$client = "BaselineReader";
 
-data <- rbind(atomicWriter, atomicReader, transactionWriter, transactionReader)
+data <- rbind(atomicWriter, atomicReader, transactionWriter, transactionReader, baselineWriter, baselineReader)
 data <- rename(data, c("V1" = "latency"))
 
 pdf("desktop-chat-latency-plots.pdf")
