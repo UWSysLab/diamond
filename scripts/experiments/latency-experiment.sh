0ae1b41b4b119548f753fe690b37f6d19717cea3
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-18 16:12:44 -0700

    Added baseline chat client to latency experiment script

--- scripts/experiments/latency-experiment.sh
+++ scripts/experiments/latency-experiment.sh
@@ -19,7 +19,26 @@ PID1=$!
 PID2=$!
 wait $PID1 $PID2
 
+DIAMOND_SRC="/home/nl35/research/diamond-src"
+PROJECT_DIR="$DIAMOND_SRC/apps/chat/BaselineChatServer"
+JAVA_BINARY="/home/nl35/research/jdk1.8.0_60/jre/bin/java"
+classpath="$PROJECT_DIR/bin:$PROJECT_DIR/libs/gson-2.3.1.jar:$PROJECT_DIR/libs/commons-pool2-2.0.jar:$PROJECT_DIR/libs/jedis-2.4.2.jar"
+$JAVA_BINARY -cp $classpath Main 2> $DIR/baseline-server.error &
+PID_SERVER=$!
+echo $PID_SERVER
+sleep 1
+
+./baseline-chat-client-wrapper.sh fixed 1000 0 verbose localhost writer > $DIR/baseline-writer.log 2> $DIR/baseline-writer.error &
+PID1=$!
+./baseline-chat-client-wrapper.sh fixed 1000 1.0 verbose localhost reader > $DIR/baseline-reader.log 2> $DIR/baseline-reader.error &
+PID2=$!
+wait $PID1 $PID2
+kill $PID_SERVER
+echo $PID_SERVER
+
 cat $DIR/transaction-writer.log | awk '$1 !~ /Summary/ { print $4 }' > $DIR/transaction-writer-latencies.txt
 cat $DIR/transaction-reader.log | awk '$1 !~ /Summary/ { print $4 }' > $DIR/transaction-reader-latencies.txt
 cat $DIR/atomic-writer.log | awk '$1 !~ /Summary/ { print $4 }' > $DIR/atomic-writer-latencies.txt
 cat $DIR/atomic-reader.log | awk '$1 !~ /Summary/ { print $4 }' > $DIR/atomic-reader-latencies.txt
+cat $DIR/baseline-writer.log | awk '$1 !~ /Summary/ { print $3 }' > $DIR/baseline-writer-latencies.txt
+cat $DIR/baseline-reader.log | awk '$1 !~ /Summary/ { print $3 }' > $DIR/baseline-reader-latencies.txt
