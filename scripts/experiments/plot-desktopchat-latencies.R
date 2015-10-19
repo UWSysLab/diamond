4d4f2b4a4d6ac08220ceee0600622f1639cb4ad5
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-19 13:52:06 -0700

    Added labels to graphs

--- scripts/experiments/plot-desktopchat-latencies.R
+++ scripts/experiments/plot-desktopchat-latencies.R
@@ -10,17 +10,26 @@ transactionNostaleReader <- read.table("desktopchat-latency/transaction-nostale-
 baselineWriter <- read.table("desktopchat-latency/baseline-writer-latencies.txt")
 baselineReader <- read.table("desktopchat-latency/baseline-reader-latencies.txt")
 
-atomicWriter$client = "AtomicW";
-atomicReader$client = "AtomicR";
-transactionStaleWriter$client = "TransStaleW";
-transactionStaleReader$client = "TransStaleR";
-transactionNostaleWriter$client = "TransNostaleW";
-transactionNostaleReader$client = "TransNostaleR";
-baselineWriter$client = "BaselineW";
-baselineReader$client = "BaselineR";
+atomicWriter$client = "AtomicWriter";
+atomicReader$client = "AtomicReader";
+transactionStaleWriter$client = "StaleTransactionWriter";
+transactionStaleReader$client = "StaleTransactionReader";
+transactionNostaleWriter$client = "TransactionWriter";
+transactionNostaleReader$client = "TransactionReader";
+baselineWriter$client = "BaselineWriter";
+baselineReader$client = "BaselineReader";
 
 data <- rbind(atomicWriter, atomicReader, transactionStaleWriter, transactionStaleReader, transactionNostaleWriter, transactionNostaleReader, baselineWriter, baselineReader)
 data <- rename(data, c("V1" = "latency"))
+
+atomicWriter$client = "Atomic";
+atomicReader$client = "Atomic";
+transactionStaleWriter$client = "Transaction (stale)";
+transactionStaleReader$client = "Transaction (stale)";
+transactionNostaleWriter$client = "Transaction";
+transactionNostaleReader$client = "Transaction";
+baselineWriter$client = "Baseline";
+baselineReader$client = "Baseline";
 
 writers <- rbind(atomicWriter, transactionStaleWriter, transactionNostaleWriter, baselineWriter)
 writers <- rename(writers, c("V1" = "latency"))
@@ -30,13 +39,13 @@ readers <- rename(readers, c("V1" = "latency"))
 
 pdf("desktop-chat-latency-plots.pdf")
 
-ggplot(data, aes(x=latency, color=factor(client))) + stat_ecdf() + coord_cartesian(xlim=c(0, 0.7))
+ggplot(data, aes(x=latency, color=factor(client))) + stat_ecdf() + coord_fixed(ratio=0.33, xlim=c(0, 0.7)) + labs(x = "Latency (ms)", y = "CDF", color = "")
 #ggplot(data, aes(x=latency, color=factor(client))) + stat_ecdf()
 
-ggplot(readers, aes(x=latency, color=factor(client))) + stat_ecdf() + coord_cartesian(xlim=c(0, 0.7))
+ggplot(readers, aes(x=latency, color=factor(client))) + stat_ecdf() + coord_fixed(ratio=0.33, xlim=c(0, 0.7)) + labs(x = "Latency (ms)", y = "CDF", color = "")
 #ggplot(readers, aes(x=latency, color=factor(client))) + stat_ecdf()
 
-ggplot(writers, aes(x=latency, color=factor(client))) + stat_ecdf() + coord_cartesian(xlim=c(0, 0.7))
+ggplot(writers, aes(x=latency, color=factor(client))) + stat_ecdf() + coord_fixed(ratio=0.33, xlim=c(0, 0.7)) + labs(x = "Latency (ms)", y = "CDF", color = "")
 #ggplot(writers, aes(x=latency, color=factor(client))) + stat_ecdf()
 
 #ggplot(data, aes(x=factor(client), y=latency)) + geom_boxplot() + coord_cartesian(ylim=c(0, 0.5))
--- scripts/experiments/plot-twitter-latencies.R
+++ scripts/experiments/plot-twitter-latencies.R
@@ -5,15 +5,15 @@ original <- read.table("twitter-latency/original-data.txt")
 diamond <- read.table("twitter-latency/diamond-data.txt")
 
 original$system = "original"
-diamond$system = "diamond"
+diamond$system = "Diamond"
 
 data <- rbind(original, diamond)
 data <- rename(data, c("V1" = "latency"))
 
 pdf("android-twitter-latency-plots.pdf")
 
-ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf() + coord_cartesian(xlim = c(0, 300))
-ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf() + coord_cartesian(xlim = c(0, 50))
+ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf() + coord_fixed(ratio = 30, xlim = c(0, 70)) + labs(x = "Latency (ms)", y = "CDF", color = "")
+ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf() + coord_fixed(ratio = 100, xlim = c(0, 300))
 ggplot(data, aes(x=latency, color=factor(system))) + stat_ecdf()
 
 ggplot(data, aes(x=factor(system), y=latency)) + geom_boxplot() + coord_cartesian(ylim = c(0, 50))
