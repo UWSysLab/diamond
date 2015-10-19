800dc65ab28f017858b94a665ad32e9cd217b204
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-19 09:16:24 -0700

    Latency experiment runs for a time interval instead of fixed number of actions

--- scripts/experiments/latency-experiment.sh
+++ scripts/experiments/latency-experiment.sh
@@ -7,17 +7,23 @@ SERVER=localhost
 
 DIR="desktopchat-latency"
 
-./desktop-chat-wrapper.sh fixed 1000 0 transaction verbose $SERVER writer latencyroom stale 100 > $DIR/transaction-writer.log 2> $DIR/transaction-writer.error &
+echo Transaction
+
+./desktop-chat-wrapper.sh timed 5 0 transaction verbose $SERVER writer latencyroom stale 100 > $DIR/transaction-writer.log 2> $DIR/transaction-writer.error &
 PID1=$!
-./desktop-chat-wrapper.sh fixed 1000 1.0 transaction verbose $SERVER reader latencyroom stale 100 > $DIR/transaction-reader.log 2> $DIR/transaction-reader.error &
+./desktop-chat-wrapper.sh timed 5 1.0 transaction verbose $SERVER reader latencyroom stale 100 > $DIR/transaction-reader.log 2> $DIR/transaction-reader.error &
 PID2=$!
 wait $PID1 $PID2
 
-./desktop-chat-wrapper.sh fixed 1000 0 atomic verbose $SERVER writer latencyroom nostale 0 > $DIR/atomic-writer.log 2> $DIR/atomic-writer.error &
+echo Atomic
+
+./desktop-chat-wrapper.sh timed 5 0 atomic verbose $SERVER writer latencyroom nostale 0 > $DIR/atomic-writer.log 2> $DIR/atomic-writer.error &
 PID1=$!
-./desktop-chat-wrapper.sh fixed 1000 1.0 atomic verbose $SERVER reader latencyroom nostale 0 > $DIR/atomic-reader.log 2> $DIR/atomic-reader.error &
+./desktop-chat-wrapper.sh timed 5 1.0 atomic verbose $SERVER reader latencyroom nostale 0 > $DIR/atomic-reader.log 2> $DIR/atomic-reader.error &
 PID2=$!
 wait $PID1 $PID2
+
+echo Baseline
 
 DIAMOND_SRC="/home/nl35/research/diamond-src"
 PROJECT_DIR="$DIAMOND_SRC/apps/chat/BaselineChatServer"
@@ -25,16 +31,14 @@ JAVA_BINARY="/home/nl35/research/jdk1.8.0_60/jre/bin/java"
 classpath="$PROJECT_DIR/bin:$PROJECT_DIR/libs/gson-2.3.1.jar:$PROJECT_DIR/libs/commons-pool2-2.0.jar:$PROJECT_DIR/libs/jedis-2.4.2.jar"
 $JAVA_BINARY -cp $classpath Main 2> $DIR/baseline-server.error &
 PID_SERVER=$!
-echo $PID_SERVER
 sleep 1
 
-./baseline-chat-client-wrapper.sh fixed 1000 0 verbose localhost writer > $DIR/baseline-writer.log 2> $DIR/baseline-writer.error &
+./baseline-chat-client-wrapper.sh timed 5 0 verbose localhost writer > $DIR/baseline-writer.log 2> $DIR/baseline-writer.error &
 PID1=$!
-./baseline-chat-client-wrapper.sh fixed 1000 1.0 verbose localhost reader > $DIR/baseline-reader.log 2> $DIR/baseline-reader.error &
+./baseline-chat-client-wrapper.sh timed 5 1.0 verbose localhost reader > $DIR/baseline-reader.log 2> $DIR/baseline-reader.error &
 PID2=$!
 wait $PID1 $PID2
 kill $PID_SERVER
-echo $PID_SERVER
 
 cat $DIR/transaction-writer.log | awk '$1 !~ /Summary/ { print $4 }' > $DIR/transaction-writer-latencies.txt
 cat $DIR/transaction-reader.log | awk '$1 !~ /Summary/ { print $4 }' > $DIR/transaction-reader-latencies.txt
