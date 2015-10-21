fb88d8e337274ccf5617615985cab5820b557f61
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-21 14:51:12 -0700

    Update Android latency graph script

--- scripts/experiments/copy-graphs.pl
+++ scripts/experiments/copy-graphs.pl
@@ -3,6 +3,6 @@
 my $dir = "/home/nl35/research/paper-diamond/figures";
 
 system("cp android-twitter-latency-plots.pdf $dir");
-system("cp desktop-chat-latency-plots.pdf $dir");
 system("cp desktop-chat-throughput-plots.pdf $dir");
-system("cp android-chat-latency-plots.pdf $dir");
+system("cp android-chat-latency-readers.pdf $dir");
+system("cp android-chat-latency-writers.pdf $dir");
