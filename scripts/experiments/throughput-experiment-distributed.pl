1bf9ef692fefee07a5570f231b3fe137678a17d2
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-23 09:46:01 -0700

    Run HTTP servers on multiple machines (4 on each machine)

--- scripts/experiments/throughput-experiment-distributed.pl
+++ scripts/experiments/throughput-experiment-distributed.pl
@@ -5,9 +5,9 @@ use strict;
 
 my $time = 5;
 my $warmupTimeMs = 5000;
+my $startingNumClients = 5;
 my $maxClients = 40;
 my $numClientsStep = 5;
-my $startingNumClients = 5;
 my $readFraction = 0.9;
 
 my $dir = "desktopchat-throughput";
@@ -15,9 +15,9 @@ my $prefix = "$dir/run";
 
 my $diamondServer = "moranis.cs.washington.edu";
 
-my $baselineServer = "spyhunter.cs.washington.edu";
+my @baselineServers = ("spyhunter.cs.washington.edu", "breakout.cs.washington.edu", "tradewars.cs.washington.edu", "zork.cs.washington.edu");
 my @clientMachines = ("qbert.cs.washington.edu", "pitfall.cs.washington.edu", "mandel.cs.washington.edu", "thompson.cs.washington.edu",
-                      "toronto.cs.washington.edu", "zork.cs.washington.edu", "chong.cs.washington.edu", "foley.cs.washington.edu");
+                      "toronto.cs.washington.edu", "short.cs.washington.edu", "chong.cs.washington.edu", "foley.cs.washington.edu");
 
 my $concurrency = "transaction";
 my $staleness = "nostale";
@@ -49,9 +49,12 @@ doBaselineExperiment();
 parseThroughputs();
 
 sub checkBaselineServers {
-    my $pids = `ssh $baselineServer ps aux | grep baseline-server | grep java | awk '{print \$2}'`;
-    my @pids = split(/\s+/, $pids);
-    my $numServers = scalar(@pids);
+    my $numServers = 0;
+    for my $server (@baselineServers) {
+        my $pids = `ssh $server ps aux | grep baseline-server | grep java | awk '{print \$2}'`;
+        my @pids = split(/\s+/, $pids);
+        $numServers = $numServers + scalar(@pids);
+    }
     if ($numServers > 0) {
         die "Error: some baseline chat servers from the last round are still alive";
     }
@@ -78,10 +81,12 @@ sub getNumBaselineClients {
 }
 
 sub killBaselineServers {
-    my $pids = `ssh $baselineServer ps aux | grep baseline-server | grep java | awk '{print \$2}'`;
-    my @pids = split(/\s+/, $pids);
-    for my $pid (@pids) {
-        system("ssh $baselineServer kill $pid");
+    for my $server (@baselineServers) {
+        my $pids = `ssh $server ps aux | grep baseline-server | grep java | awk '{print \$2}'`;
+        my @pids = split(/\s+/, $pids);
+        for my $pid (@pids) {
+            system("ssh $server kill $pid");
+        }
     }
 }
 
@@ -167,15 +172,18 @@ sub doExperiment {
 
 sub doBaselineExperiment {
     print("Starting baseline servers\n");
-    system("ssh $baselineServer /homes/sys/nl35/research/chat-program-package/baseline-server/baseline-server-package-wrapper.sh 9000 /homes/sys/nl35/research/chat-program-package &");
-    system("ssh $baselineServer /homes/sys/nl35/research/chat-program-package/baseline-server/baseline-server-package-wrapper.sh 9001 /homes/sys/nl35/research/chat-program-package &");
-    system("ssh $baselineServer /homes/sys/nl35/research/chat-program-package/baseline-server/baseline-server-package-wrapper.sh 9002 /homes/sys/nl35/research/chat-program-package &");
-    system("ssh $baselineServer /homes/sys/nl35/research/chat-program-package/baseline-server/baseline-server-package-wrapper.sh 9003 /homes/sys/nl35/research/chat-program-package &");
+    for (my $i = 0; $i < @baselineServers; $i++) {
+        for (my $j = 0; $j < 4; $j++) {
+            my $server = $baselineServers[$i];
+            my $port = 9000 + $j;
+            system("ssh $server /homes/sys/nl35/research/chat-program-package/baseline-server/baseline-server-package-wrapper.sh $port /homes/sys/nl35/research/chat-program-package &");
+        }
+    }
 
     sleep(1);
 
     # fill chat log
-    system("ssh $clientMachines[0] research/chat-program-package/baseline-client/baseline-client-package-wrapper.sh fixed 200 0.0 concise $baselineServer 9000 filler 0 research/chat-program-package");
+    system("ssh $clientMachines[0] research/chat-program-package/baseline-client/baseline-client-package-wrapper.sh fixed 200 0.0 concise $baselineServers[0] 9000 filler 0 research/chat-program-package");
 
     open(FILE, "> $log");
     for (my $numClients = $startingNumClients; $numClients <= $maxClients; $numClients += $numClientsStep) {
@@ -184,9 +192,12 @@ sub doBaselineExperiment {
         system("rm $prefix.*");
 
         for (my $i = 0; $i < $numClients; $i++) {
-            my $port = 9000 + $i % 4;
+            my $serverNum = $i % @baselineServers;
+            my $portOffset = ($i / @baselineServers) % 4;
+            my $server = $baselineServers[$serverNum];
+            my $port = 9000 + $portOffset;
             my $clientMachine = @clientMachines[$i % scalar(@clientMachines)];
-            system("ssh $clientMachine \"research/chat-program-package/baseline-client/baseline-client-package-wrapper.sh timed $time $readFraction concise $baselineServer $port client$i $warmupTimeMs research/chat-program-package\" > $prefix.$i.log 2> $prefix.$i.error &");
+            system("ssh $clientMachine \"research/chat-program-package/baseline-client/baseline-client-package-wrapper.sh timed $time $readFraction concise $server $port client$i $warmupTimeMs research/chat-program-package\" > $prefix.$i.log 2> $prefix.$i.error &");
         }
 
         sleep(1);
