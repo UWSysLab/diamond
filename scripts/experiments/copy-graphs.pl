1ef23012f7090d3bd99cf037fd440e82e08794cf
Niel Lebeck <niel.lebeck@gmail.com>
2015-10-22 18:43:20 -0700

    More script updates

--- scripts/experiments/copy-graphs.pl
+++ scripts/experiments/copy-graphs.pl
@@ -2,7 +2,11 @@
 
 my $dir = "/home/nl35/research/paper-diamond/figures";
 
-system("cp android-twitter-latency-plots.pdf $dir");
-system("cp desktop-chat-throughput-plots.pdf $dir");
+system("pdfcrop android-twitter-latency-plots.pdf");
+system("mv android-twitter-latency-plots-crop.pdf $dir/android-twitter-latency-plots.pdf");
+
+system("pdfcrop desktop-chat-throughput-plots.pdf");
+system("mv desktop-chat-throughput-plots-crop.pdf $dir/desktop-chat-throughput-plots.pdf");
+
 system("cp android-chat-latency-readers.pdf $dir");
 system("cp android-chat-latency-writers.pdf $dir");
