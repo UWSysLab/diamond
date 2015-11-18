92e196a3cc604a331f318c9eeb3ee0c30dd4dc79
Niel Lebeck <niel.lebeck@gmail.com>
2015-11-18 09:40:14 -0800

    Changed DiamondAndroidTest back to basic test behavior

--- apps/test-apps/DiamondAndroidTest/src/com/example/diamondandroidtest/MainActivity.java
+++ apps/test-apps/DiamondAndroidTest/src/com/example/diamondandroidtest/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ActionBarActivity {
 		
 		Diamond.DiamondInit("coldwater.cs.washington.edu");
 		
-		/*Diamond.DString testString1 = new Diamond.DString("String 1: if this string shows up, Diamond syncing is not working", "a");
+		Diamond.DString testString1 = new Diamond.DString("String 1: if this string shows up, Diamond syncing is not working", "a");
 		Diamond.DString testString2 = new Diamond.DString("String 2: if this string shows up, Diamond syncing is not working", "a");
 
 		testString2.Set(new String("Testing Diamond on Android: syncing appears to work"));
@@ -25,12 +25,13 @@ public class MainActivity extends ActionBarActivity {
 		TextView testTextBox = new TextView(getBaseContext());
 		testTextBox.setText(testString1.Value());
 		testTextBox.setTextColor(Color.BLACK);
-		setContentView(testTextBox);*/
+		setContentView(testTextBox);
 		
-		Diamond.DString testString = new Diamond.DString();
+		/*Diamond.DString testString = new Diamond.DString();
 		Diamond.DObject.Map(testString, "a");
 		Log.e("DEBUG", "String: " + testString.Value());
-		Log.e("DEBUG", "String: " + testString.Value());
+		Log.e("DEBUG", "String: " + testString.Value());*/
+		
 		//Diamond.DString testString = new Diamond.DString("Testing", "a");
 		//testString.Set("Testing");
 		//TextView testTextBox = new TextView(getBaseContext());
