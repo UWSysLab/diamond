4e83c3318fec64904097e6abdbd723b563b8362a
Niel Lebeck <niel.lebeck@gmail.com>
2015-12-04 11:07:27 -0800

    Wrote a header file and dummy C++ library implementation for notifyReactiveUpcall()

---
+++ backend/src/bindings/cpp/diamondnotifycpp.cc
@@ -0,0 +1,7 @@
+#include "includes/notify_reactive.h"
+
+#include "lib/message.h"
+
+void notifyReactiveUpcall() {
+    Notice("Reactive call into C++ code\n");
+}
---
+++ backend/src/includes/notify_reactive.h
@@ -0,0 +1,6 @@
+#ifndef _NOTIFY_REACTIVE_H
+#define _NOTIFY_REACTIVE_H
+
+void notifyReactiveUpcall();
+
+#endif
