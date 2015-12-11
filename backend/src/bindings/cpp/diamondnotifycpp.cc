839676c540693e6e016bb25b99a8f8a9a91d6196
Niel Lebeck <niel.lebeck@gmail.com>
2015-12-10 17:20:53 -0800

    Removed shared library-based diamondnotify code

--- backend/src/bindings/cpp/diamondnotifycpp.cc
+++
@@ -1,9 +0,0 @@
-#include "includes/diamondnotify.h"
-
-#include <iostream>
-
-#include "lib/message.h"
-
-void notifyReactiveUpcall() {
-    std::cout << "Reactive call into C++ code\n" << std::endl;
-}
--- backend/src/includes/diamondnotify.h
+++
@@ -1,6 +0,0 @@
-#ifndef _NOTIFY_REACTIVE_H
-#define _NOTIFY_REACTIVE_H
-
-void notifyReactiveUpcall();
-
-#endif
