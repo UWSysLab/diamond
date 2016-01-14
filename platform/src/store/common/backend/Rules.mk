f3eaaadb05a3dd3707842ed84b7d6c850f65b19a
Irene Zhang <iyzhang@zookeeper.cs.washington.edu>
2016-01-14 15:58:24 -0800

    making new CMake files and moving lots of things around

--- platform/src/store/common/backend/Rules.mk
+++
@@ -1,8 +0,0 @@
-d := $(dir $(lastword $(MAKEFILE_LIST)))
-
-SRCS += $(addprefix $(d), \
-				kvstore.cc lockserver.cc txnstore.cc versionstore.cc)
-
-LIB-store-backend := $(o)kvstore.o $(o)lockserver.o $(o)txnstore.o $(o)versionstore.o
-
-include $(d)tests/Rules.mk
--- platform/src/store/common/frontend/Rules.mk
+++
@@ -1,5 +0,0 @@
-d := $(dir $(lastword $(MAKEFILE_LIST)))
-
-SRCS += $(addprefix $(d), bufferclient.cc txnclient.cc client.cc)
-
-LIB-store-frontend := $(o)bufferclient.o $(o)txnclient.o $(o)client.o
