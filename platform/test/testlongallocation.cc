45c19de2895e341abfa699fafa79e17b0dd7d6e5
Irene Zhang <iyzhang@zookeeper.cs.washington.edu>
2016-01-18 10:55:54 -0800

    adding test cases

---
+++ platform/test/testlongallocation.cc
@@ -0,0 +1,26 @@
+#include "includes/data_types.h"
+
+using namespace diamond;
+
+int main(void){
+    DiamondInit();
+
+    DLong long1;
+    DObject::Map(long1, "unusedkey");
+
+    long lastReadValue = 0;
+
+    int committed = 0;
+    while(true) {
+        DObject::TransactionBegin();
+        if (lastReadValue == long1.Value()) {
+            DObject::TransactionRetry();
+            continue;
+        }
+        lastReadValue = long1.Value();
+        DObject::TransactionCommit();
+    }
+
+    return 0;
+}
+
