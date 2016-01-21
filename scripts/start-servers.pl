51a5307638c7b2aa07f996e186c3f7654f235344
Niel Lebeck <niel.lebeck@gmail.com>
2016-01-20 16:33:46 -0800

    Usage

--- scripts/start-servers.pl
+++ scripts/start-servers.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-die "./start-servers.pl config_file" unless @ARGV==1;
+die "usage: ./start-servers.pl config_file" unless @ARGV==1;
 my $config = shift;
 
 system("../platform/build/server -c $config -i 0 &");
