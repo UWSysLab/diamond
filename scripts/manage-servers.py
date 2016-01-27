67845a4ec185f5146dd29dca7377a6017b52f0a8
Niel Lebeck <niel.lebeck@gmail.com>
2016-01-27 14:16:46 -0800

    Script handles multiple shards

--- scripts/manage-servers.py
+++ scripts/manage-servers.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import re
 import time
+import sys
 
 USER_NAME = "nl35"
 WORKING_DIR = "/biggerraid/users/" + USER_NAME + "/scratch"
@@ -14,30 +15,46 @@ parser.add_argument('action', choices=['start', 'kill'], help='the action to tak
 parser.add_argument('config_prefix', help='the config file prefix')
 args = parser.parse_args()
 
-serverConfigPath = args.config_prefix + '0.config'
 serverExecutablePath = BUILD_DIR + "/server"
-tssConfigPath = args.config_prefix + '.tss.config'
+tssConfigPath = args.config_prefix + ".tss.config"
 tssExecutablePath = BUILD_DIR + "/tss"
 
-remoteServerConfigPath = WORKING_DIR + "/diamond0.config"
 remoteServerExecutablePath = WORKING_DIR + "/server"
 remoteTssConfigPath = WORKING_DIR + "/diamond.tss.config"
 remoteTssExecutablePath = WORKING_DIR + "/tss"
 
-serverConfig = open(serverConfigPath, 'r')
-i = 0
-for line in serverConfig:
-    match = re.match("replica\s+([\w\.]+):(\d)", line)
-    if match:
-        hostname = match.group(1)
-        if args.action == 'start':
-            os.system("scp " + serverConfigPath + " " + hostname + ":" + remoteServerConfigPath)
-            os.system("scp " + serverExecutablePath + " " + hostname + ":" + remoteServerExecutablePath)
-            os.system("ssh -f " + hostname + " '" + remoteServerExecutablePath + " -c " + remoteServerConfigPath + " -i " + repr(i) + "'");
-        elif args.action == 'kill':
-            os.system("ssh " + hostname + " 'pkill " + remoteServerConfigPath + "'");
-        i = i + 1
+# find number of shards
+configPath = args.config_prefix + "0.config"
+config = open(configPath, 'r')
+line = config.readline()
+match = re.match("f\s+(\d+)", line)
+if match:
+    numShards = int(match.group(1))
+    print "Number of shards: " + repr(numShards)
+else:
+    print "Error: could not get number of shards"
+    sys.exit
 
+# launch servers
+for shardNum in range(0, numShards):
+    serverConfigPath = args.config_prefix + repr(shardNum) + ".config"
+    remoteServerConfigPath = WORKING_DIR + "/diamond" + repr(shardNum) + ".config"
+    serverConfig = open(serverConfigPath, 'r')
+    replicaNum = 0
+    for line in serverConfig:
+        match = re.match("replica\s+([\w\.]+):(\d)", line)
+        if match:
+            hostname = match.group(1)
+            if args.action == 'start':
+                os.system("scp " + serverConfigPath + " " + hostname + ":" + remoteServerConfigPath)
+                os.system("scp " + serverExecutablePath + " " + hostname + ":" + remoteServerExecutablePath)
+                os.system("ssh -f " + hostname + " '" + remoteServerExecutablePath + " -c " + remoteServerConfigPath + " -i " + repr(replicaNum) + "'");
+            elif args.action == 'kill':
+                os.system("ssh " + hostname + " 'pkill " + remoteServerConfigPath + "'");
+            replicaNum = replicaNum + 1
+    serverConfig.close()
+
+# launch tss servers
 tssConfig = open(tssConfigPath, 'r')
 i = 0
 for line in tssConfig:
@@ -47,7 +64,8 @@ for line in tssConfig:
         if args.action == 'start':
             os.system("scp " + tssConfigPath + " " + hostname + ":" + remoteTssConfigPath)
             os.system("scp " + tssExecutablePath + " " + hostname + ":" + remoteTssExecutablePath)
-            os.system("ssh -f " + hostname + " '" + remoteTssExecutablePath + " -c " + remoteTssConfigPath + " -i " + repr(i) + "'");
+            os.system("ssh -f " + hostname + " '" + remoteTssExecutablePath + " -c " + remoteTssConfigPath + " -i " + repr(replicaNum) + "'");
         elif args.action == 'kill':
             os.system("ssh " + hostname + " 'pkill " + remoteTssConfigPath + "'");
-        i = i + 1
+        replicaNum = replicaNum + 1
+tssConfig.close()
