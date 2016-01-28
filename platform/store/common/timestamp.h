414257f21b1ca6fc84db90f906150819ffd349d5
Irene Y Zhang <iyzhang@cs.washington.edu>
2016-01-28 15:58:29 -0800

    adding timestamp format file

---
+++ platform/store/common/timestamp.h
@@ -0,0 +1,15 @@
+// -*- mode: c++; c-file-style: "k&r"; c-basic-offset: 4 -*-
+// vim: set ts=4 sw=4:
+/***********************************************************************
+ *
+ * common/timestamp.h:
+ *   Our timestamp definition
+ *
+ **********************************************************************/
+
+#ifndef _TIMESTAMP_H
+#define _TIMESTAMP_H_
+
+typedef uint64_t Timestamp;
+
+#endif /* _TIMESTAMP_H_ */
