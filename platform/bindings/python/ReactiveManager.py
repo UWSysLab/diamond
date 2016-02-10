ec1b597c258ad8185d9e5ad1ac551aaed7586c83
Niel Lebeck <niel.lebeck@gmail.com>
2016-02-10 11:26:02 -0800

    Added python txn_execute() method and switched back to Python threads (instead of Twisted)

--- platform/bindings/python/ReactiveManager.py
+++ platform/bindings/python/ReactiveManager.py
@@ -5,31 +5,39 @@ sys.path.append("../../build/bindings/python")
 from libpydiamond import *
 from twisted.internet import reactor
 
-txSet = set()
+txDict = dict()
 
 def runInBackground(target, *args, **kwargs):
-    #threading.Thread(target=target, args=args, kwargs=kwargs).start()
-    reactor.callInThread(target, *args, **kwargs)
+    threading.Thread(target=target, args=args, kwargs=kwargs).start()
+    #reactor.callInThread(target, *args, **kwargs)
 
 def start():
-    #thread = threading.Thread(target=run, args=())
-    #thread.daemon = True
-    #thread.start()
-    reactor.callInThread(run)
+    thread = threading.Thread(target=run, args=())
+    thread.daemon = True
+    thread.start()
+    #reactor.callInThread(run)
 
 def run():
     while True:
         time.sleep(1)
-        for func in txSet:
+        for func in txDict.keys():
             DObject.TransactionBegin()
-            func()
+            func(*txDict[func])
             DObject.TransactionCommit()
 
-def add(func):
-    txSet.add(func)
+def add(func, *args):
+    txDict[func] = args
 
 def remove(func):
-    txSet.remove(func)
+    del txDict[func]
+
+def txn_execute(func, *args):
+    runInBackground(txn_execute_helper, func, *args)
+
+def txn_execute_helper(func, *args):
+    DObject.TransactionBegin()
+    func(*args)
+    DObject.TransactionCommit()
 
 start()
 
