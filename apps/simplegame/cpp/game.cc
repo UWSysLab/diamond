543ba380b4fe4213f928c825c88b4f6b5204bc7e
Niel Lebeck <niel.lebeck@gmail.com>
2016-02-11 17:10:54 -0800

    Started on C++ version of game to test execute_txn()

---
+++ apps/simplegame/cpp/game.cc
@@ -0,0 +1,52 @@
+#include <boost/program_options.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+#include <includes/data_types.h>
+#include <includes/transactions.h>
+
+namespace po = boost::program_options;
+using namespace diamond;
+
+void myVariadicFunction(va_list args) {
+    int * num = va_arg(args, int *);
+    std::cout << "We are Diamond Dogs: " << *num << std::endl;
+    *num = 3;
+}
+
+void myFunction(const std::string & str) {
+    std::cout << "I don't know what I'm doing, but I got a string argument: " << str << std::endl;
+}
+
+int main(int argc, char ** argv) {
+    std::string configPrefix;
+    std::string name;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("name", po::value<std::string>(&name)->required(), "name to use in the game")
+        ("config", po::value<std::string>(&configPrefix)->required(), "frontend config file prefix (required)")
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+    po::notify(vm);
+
+    DiamondInit(configPrefix, 1, 0);
+
+    //std::function<void()> funcObj = std::bind(myFunction, "stringcheese");
+    //execute_txn(funcObj);
+    int num = 2;
+    std::string str("lambdas are cool");
+    execute_txn([str] () { std::cout << "Lambda string time: " << str << std::endl; } );
+    //std::cout << "Variadic functions sent us to hell, but we're going even deeper: " << num << std::endl;
+
+    while(true) {
+    }
+}
