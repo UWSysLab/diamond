2ccc55fc4af267918fe5fc333318de0e1444142d
Niel Lebeck <niel.lebeck@gmail.com>
2016-02-29 15:44:48 -0800

    Changed C++ game to use callbacks to detect aborts and did misc cleanup/fixes

--- apps/simplegame/cpp/game.cc
+++ apps/simplegame/cpp/game.cc
@@ -44,32 +44,37 @@ int main(int argc, char ** argv) {
     DObject::Map(currentMove, "100game:move");
 
     // Add user to the game
-    int ret = execute_txn([myName] () {
+    execute_txn([myName] () {
         if (players.Index(myName) == -1) {
             players.Append(myName);
         }
+    },
+    [] (int committed) { // If transaction fails, exit
+        if (!committed) {
+            cout << "Failed adding player to game\n";
+            exit(1);
+        }
     });
-    // If transaction fails, exit
-    // TODO: note from Niel: does ret have any meaningful info, or should we use a callback here?
-    if (ret != 0) return -1;
 
     // Set up our reactive print outs
-    txn_id id = reactive_txn([myName] () {
-         cout << "Current total: " << score.Value() << "\n";
-         string cp = players[currentMove.Value() % players.Size()];  
-         if (score.Value() >= 100)
-            cout << cp << " won! Game Over!\n";
-         else if (cp == myName)
-            cout << " Enter number between 1 and 10: \n";
-         else
-            cout << " It's " << cp << "'s turn. \n";
+    uint64_t reactive_id = reactive_txn([myName] () {
+        cout << "Current total: " << score.Value() << "\n";
+        if (players.Size() > 0) {
+            string cp = players[currentMove.Value() % players.Size()];  
+            if (score.Value() >= 100)
+                cout << cp << " won! Game Over!\n";
+            else if (cp == myName)
+                cout << " Enter number between 1 and 10: \n";
+            else
+                cout << " It's " << cp << "'s turn. \n";
+        }
     });
 
     // Cycle on user input
     while (1) {
         int inc = 0;
         cin >> inc;
-        int ret = execute_txn([myName, inc] () {
+        execute_txn([myName, inc] () {
 	    string cp = players[currentMove.Value() % players.Size()];
 	    // If it's the user's turn, make move
 	    if (cp == myName && inc >= 1 && inc <= 10) {
@@ -78,12 +83,15 @@ int main(int argc, char ** argv) {
 	    } else {
 	       abort_txn();
 	    }
-	});
-        // If we can't make a move, just exit
-        if (ret != 0) {
-	   reactive_stop(id);
-	   return -1;
+	},
+        [reactive_id] (int committed) { // If we can't make a move, just exit
+            if (!committed) {
+                cout << "Taking turn failed\n";
+                reactive_stop(reactive_id);
+                exit(1);
+            }
         }
+        );
     }
     return 0;
 }
