be262addccf6e1c1e4e11db84da1246388ab3d8c
Niel Lebeck <niel.lebeck@gmail.com>
2016-03-29 14:10:21 -0700

    scalability-experiment.pl counts the number of output files (to detect random number collisions or client failures)

--- scripts/gce/docker/scalability-experiment.pl
+++ scripts/gce/docker/scalability-experiment.pl
@@ -10,13 +10,15 @@ system("rm -f $log; touch $log");
 system("ssh 104.154.73.35 'cd diamond-src/apps/benchmarks/build; git pull origin master; make -j8' >> $log 2>&1");
 system("./build-kubernetes.pl nielgce nl35 >> $log 2>&1");
 
-print("instances\tthroughput\tlatency\tabortrate\tseconds\n");
+print("instances\tthroughput\tlatency\tabortrate\tseconds\tcount\n");
 
 my @instanceNums = (1, 5, 10, 20, 50, 100);
 
 for my $instances (@instanceNums) {
     system("ssh 104.154.73.35 'rm diamond-src/scripts/experiments/scalability/*' >> $log 2>&1");
     system("./run-kubernetes-job.pl scaling nielgce run_scalability.py nl35 $instances >> $log 2>&1");
+    my $count = `ssh 104.154.73.35 'ls diamond-src/scripts/experiments/scalability | wc' | awk '{ print \$1 }'`;
+    chomp($count);
     my @result = `ssh 104.154.73.35 'cd diamond-src/scripts/experiments; ./parse-scalability.py scalability'`;
 
     my $throughput = "ERROR";
@@ -37,5 +39,5 @@ for my $instances (@instanceNums) {
             $seconds = $1;
         }
     }
-    print("$instances\t$throughput\t$latency\t$abortrate\t$seconds\n");
+    print("$instances\t$throughput\t$latency\t$abortrate\t$seconds\t$count\n");
 }
