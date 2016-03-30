6b08b2a2bde225f084b8549ce3f504bf85645c21
Niel Lebeck <niel.lebeck@gmail.com>
2016-03-30 12:13:23 -0700

    Changed run_scalability.py to spread clients across frontends

--- scripts/gce/docker/run_scalability.py
+++ scripts/gce/docker/run_scalability.py
@@ -11,19 +11,22 @@ WORKING_DIR = "~/"
 
 COPY_CMD = "rsync " + SRC_HOST + ":diamond-src/"
 CONFIG_DIR = "platform/test/"
-CONFIG_FILE = "gce.frontend"
+CONFIG_PREFIX = "gce.frontend"
 KEY_DIR = "scripts/experiments/"
 KEY_FILE = "keys.txt"
 NUM_KEYS = "1000"
 NUM_SECONDS = "120"
 OUTPUT_DEST = "scripts/experiments/scalability/"
 NUM_CLIENTS = 10
+NUM_FRONTENDS = 5
 
 os.system(COPY_CMD + "apps/benchmarks/build/scalability" + " " + WORKING_DIR)
 os.system(COPY_CMD + "platform/build/libdiamond.so" + " " + WORKING_DIR)
 
 # Copy config files
-os.system(COPY_CMD + CONFIG_DIR + CONFIG_FILE + ".config" + " " + WORKING_DIR)
+for i in range(0, NUM_FRONTENDS):
+    configFile = CONFIG_PREFIX + repr(i)
+    os.system(COPY_CMD + CONFIG_DIR + configFile + ".config" + " " + WORKING_DIR)
 os.system(COPY_CMD + KEY_DIR + KEY_FILE + " " + WORKING_DIR)
 
 # Run binary
@@ -31,7 +34,8 @@ processes = []
 outputFiles = []
 for i in range(0, NUM_CLIENTS):
     outputFile = "scalability-out-" + repr(random.randint(0, sys.maxint))
-    cmd = WORKING_DIR + "/scalability --config " + WORKING_DIR + CONFIG_FILE + " --keys " + WORKING_DIR + KEY_FILE + " --numkeys " + NUM_KEYS + " --time " + NUM_SECONDS + " > " + WORKING_DIR + outputFile
+    configFile = CONFIG_PREFIX + repr(i % NUM_FRONTENDS)
+    cmd = WORKING_DIR + "/scalability --config " + WORKING_DIR + configFile + " --keys " + WORKING_DIR + KEY_FILE + " --numkeys " + NUM_KEYS + " --time " + NUM_SECONDS + " > " + WORKING_DIR + outputFile
     processes.append(subprocess.Popen(cmd, shell=True))
     outputFiles.append(outputFile)
 
