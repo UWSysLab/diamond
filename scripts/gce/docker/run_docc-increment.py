e6625c79efd9a37ec1834f514cb30a94f1cc3272
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-01 12:10:17 -0700

    Renamed contention experiment to docc

--- scripts/gce/docker/run_docc-increment.py
+++ scripts/gce/docker/run_docc-increment.py
@@ -16,11 +16,11 @@ KEY_DIR = "scripts/experiments/"
 KEY_FILE = "keys.txt"
 NUM_KEYS = "100"
 NUM_SECONDS = "120"
-OUTPUT_DEST = "scripts/experiments/contention-noincrement/"
+OUTPUT_DEST = "scripts/experiments/docc-increment/"
 NUM_CLIENTS = 10
 NUM_FRONTENDS = 10
 
-os.system(COPY_CMD + "apps/benchmarks/build/contention" + " " + WORKING_DIR)
+os.system(COPY_CMD + "apps/benchmarks/build/docc" + " " + WORKING_DIR)
 os.system(COPY_CMD + "platform/build/libdiamond.so" + " " + WORKING_DIR)
 
 # Copy config files
@@ -33,9 +33,9 @@ os.system(COPY_CMD + KEY_DIR + KEY_FILE + " " + WORKING_DIR)
 processes = []
 outputFiles = []
 for i in range(0, NUM_CLIENTS):
-    outputFile = "contention-noincrement-out-" + repr(random.randint(0, sys.maxint))
+    outputFile = "docc-increment-out-" + repr(random.randint(0, sys.maxint))
     configFile = CONFIG_PREFIX + repr(i % NUM_FRONTENDS)
-    cmd = WORKING_DIR + "/contention --config " + WORKING_DIR + configFile + " --keys " + WORKING_DIR + KEY_FILE + " --numkeys " + NUM_KEYS + " --time " + NUM_SECONDS + " > " + WORKING_DIR + outputFile
+    cmd = WORKING_DIR + "/docc --config " + WORKING_DIR + configFile + " --keys " + WORKING_DIR + KEY_FILE + " --numkeys " + NUM_KEYS + " --time " + NUM_SECONDS + " --increment > " + WORKING_DIR + outputFile
     processes.append(subprocess.Popen(cmd, shell=True))
     outputFiles.append(outputFile)
 
