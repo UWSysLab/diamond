536296559f2f8c1183909f0ef53ef59fe0142d89
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-06 23:34:45 +0000

    generate-frontend-configs.pl spreads frontends across hostnames (hardcoded for now)

--- platform/test/generate-frontend-configs.pl
+++ platform/test/generate-frontend-configs.pl
@@ -6,14 +6,24 @@
 use warnings;
 use strict;
 
-my $usage = "usage: ./generate-frontend-configs.pl num-frontends prefix hostname startport";
-die $usage unless @ARGV == 4;
+my $usage = "usage: ./generate-frontend-configs.pl num-frontends prefix";
+die $usage unless @ARGV == 2;
 
-my ($num, $prefix, $hostname, $startport) = @ARGV;
+my @hosts = ("diamond-frontend-central-8uwj",
+             "diamond-frontend-central-v02w",
+             "diamond-frontend-central-vjqi",
+             "diamond-frontend-central-wn4w",
+             "diamond-frontend-central-yk5j");
+
+my $startport = 12345;
+
+my ($num, $prefix) = @ARGV;
 
 for (my $i = 0; $i < $num; $i++) {
+    my $hostname = $hosts[$i % scalar(@hosts)];
+    my $port = $startport + int($i / scalar(@hosts));
+
     my $file = "> $prefix.frontend$i.config";
-    my $port = $startport + $i;
     open(FILE, $file);
     print(FILE "f 1\n");
     print(FILE "replica $hostname:$port\n");
