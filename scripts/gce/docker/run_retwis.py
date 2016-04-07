5ba7fe36a001d85746e1b6004349c6d66d9160d3
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-07 16:22:10 -0700

    Added code to put retwis results into redis

--- scripts/gce/docker/run_retwis.py
+++ scripts/gce/docker/run_retwis.py
@@ -7,14 +7,35 @@ NUM_KEYS = "1000"
 NUM_SECONDS = "60"
 OUTPUT_DEST = "scripts/experiments/retwis/"
 NUM_CLIENTS = 10
-USE_REDIS = False
+USE_REDIS = True
 
 def getCommandFunc(workingDir, configFile, keyFile):
     return workingDir + "/retwisClient -m diamond -c " + workingDir + configFile + " -f " + workingDir + keyFile + " -k " + NUM_KEYS + " -d " + NUM_SECONDS
 
 def processOutputFunc(outputFile):
     if USE_REDIS:
-        experiment_common.putDataInRedis(outputFile)
+        import random
+        import redis
+        import re
+        import sys
+        r = redis.StrictRedis(host=experiment_common.SRC_HOST, port=6379)
+        r.incr("clients")
+        outfile = open(outputFile, 'r')
+        for line in outfile:
+            match = re.match("^\d+\s+([\d\.]+)\s+([\d\.]+)\s+\d+\s+(\d+)\s+(\d+)", line)
+            if match:
+                startTime = int(float(match.group(1)) * 1000)
+                endTime = int(float(match.group(2)) * 1000)
+                committed = int(match.group(3))
+                type = int(match.group(4))
+                txnNum = random.randint(0, sys.maxint)
+                txnKey = "txn-" + repr(txnNum)
+                mapping = dict()
+                mapping['start-time'] = startTime
+                mapping['end-time'] = endTime
+                mapping['committed'] = committed
+                r.hmset(txnKey, mapping)
+                r.lpush("txns", txnKey)
     else:
         experiment_common.copyToSrcHost(outputFile, OUTPUT_DEST)
         
