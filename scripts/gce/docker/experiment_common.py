b6f8240440c8b26431d7209d249c54b270e38bf1
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-07 13:11:09 -0700

    Refactored common experiment code into experiment_common.py and changed run_scalability.py to use it

--- scripts/gce/docker/experiment_common.py
+++ scripts/gce/docker/experiment_common.py
@@ -1,11 +1,62 @@
 #!/usr/bin/python
 
+import os
 import random
 import re
 import redis
+import subprocess
 import sys
 
-def putDataInRedis(outputFileName, redisHost, redisPort=6379):
+SRC_HOST = "diamond-client-a1o6"
+WORKING_DIR = "~/"
+
+COPY_CMD = "rsync " + SRC_HOST + ":diamond-src/"
+CONFIG_DIR = "platform/test/"
+CONFIG_PREFIX = "gce.frontend"
+KEY_DIR = "scripts/experiments/"
+KEY_FILE = "keys.txt"
+NUM_FRONTENDS = 10
+USE_REDIS = True
+
+processes = []
+outputFiles = []
+
+def copyFiles(binary, binaryDir):
+    os.system(COPY_CMD + binaryDir + "/" + binary + " " + WORKING_DIR)
+    os.system(COPY_CMD + "platform/build/libdiamond.so" + " " + WORKING_DIR)
+    sys.stderr.write("Finished copying binaries\n")
+
+    for i in range(0, NUM_FRONTENDS):
+        configFile = CONFIG_PREFIX + repr(i)
+        os.system(COPY_CMD + CONFIG_DIR + configFile + ".config" + " " + WORKING_DIR)
+        sys.stderr.write("Finished syncing config file %d\n" % i)
+    os.system(COPY_CMD + KEY_DIR + KEY_FILE + " " + WORKING_DIR)
+    sys.stderr.write("Finished syncing key file\n")
+
+# getCommandFunc should be a function with arguments (workingDir, configFile, keyFile) that
+# returns the the shell command to execute the binary
+def runProcesses(getCommandFunc, numClients):
+    sys.stderr.write("Running clients...\n")
+
+    for i in range(0, numClients):
+        outputFile = "scalability-out-" + repr(random.randint(0, sys.maxint))
+        configFile = CONFIG_PREFIX + repr(i % NUM_FRONTENDS)
+        cmd = getCommandFunc(WORKING_DIR, configFile, KEY_FILE) + " > " + WORKING_DIR + outputFile
+        processes.append(subprocess.Popen(cmd, shell=True))
+        outputFiles.append(outputFile)
+
+    for process in processes:
+        process.wait()
+
+    sys.stderr.write("Finished running clients\n")
+
+def processOutput(processOutputFunc):
+    for outputFileName in outputFiles:
+        fullPath = os.path.expanduser(WORKING_DIR) + outputFileName
+        processOutputFunc(fullPath)
+        sys.stderr.write("Finished processing output file %s\n" % outputFileName)
+
+def putDataInRedis(outputFileName, redisHost=SRC_HOST, redisPort=6379):
     r = redis.StrictRedis(host=redisHost, port=redisPort)
     r.incr("clients")
     outputFile = open(outputFileName, 'r')
--- scripts/gce/docker/run_scalability.py
+++ scripts/gce/docker/run_scalability.py
@@ -1,66 +1,24 @@
 #!/usr/bin/python
 
-import argparse
-import os
-import random
-import subprocess
-import sys
-
 import experiment_common
+import os
 
-SRC_HOST = "diamond-client-a1o6"
-WORKING_DIR = "~/"
-
-COPY_CMD = "rsync " + SRC_HOST + ":diamond-src/"
-CONFIG_DIR = "platform/test/"
-CONFIG_PREFIX = "gce.frontend"
-KEY_DIR = "scripts/experiments/"
-KEY_FILE = "keys.txt"
 NUM_KEYS = "1000"
 NUM_SECONDS = "120"
 OUTPUT_DEST = "scripts/experiments/scalability/"
 NUM_CLIENTS = 10
-NUM_FRONTENDS = 10
 USE_REDIS = True
 
-sys.stderr.write("Testing logging to stderr\n")
+def getCommandFunc(workingDir, configFile, keyFile):
+    return workingDir + "/scalability --config " + workingDir + configFile + " --keys " + workingDir + keyFile + " --numkeys " + NUM_KEYS + " --time " + NUM_SECONDS
 
-os.system(COPY_CMD + "apps/benchmarks/build/scalability" + " " + WORKING_DIR)
-os.system(COPY_CMD + "platform/build/libdiamond.so" + " " + WORKING_DIR)
-sys.stderr.write("Finished copying binaries\n")
+def processOutputFunc(outputFile):
+    if USE_REDIS:
+        experiment_common.putDataInRedis(outputFile)
+    else:
+        os.system("rsync " + outputFile + " " + experiment_common.SRC_HOST + ":diamond-src/" + OUTPUT_DEST)
+        
 
-# Copy config files
-for i in range(0, NUM_FRONTENDS):
-    configFile = CONFIG_PREFIX + repr(i)
-    os.system(COPY_CMD + CONFIG_DIR + configFile + ".config" + " " + WORKING_DIR)
-    sys.stderr.write("Finished syncing config file %d\n" % i)
-os.system(COPY_CMD + KEY_DIR + KEY_FILE + " " + WORKING_DIR)
-sys.stderr.write("Finished syncing key file\n")
-
-# Run binary
-sys.stderr.write("Running clients...\n")
-
-processes = []
-outputFiles = []
-for i in range(0, NUM_CLIENTS):
-    outputFile = "scalability-out-" + repr(random.randint(0, sys.maxint))
-    configFile = CONFIG_PREFIX + repr(i % NUM_FRONTENDS)
-    cmd = WORKING_DIR + "/scalability --config " + WORKING_DIR + configFile + " --keys " + WORKING_DIR + KEY_FILE + " --numkeys " + NUM_KEYS + " --time " + NUM_SECONDS + " > " + WORKING_DIR + outputFile
-    processes.append(subprocess.Popen(cmd, shell=True))
-    outputFiles.append(outputFile)
-
-for process in processes:
-    process.wait()
-
-sys.stderr.write("Finished running clients\n")
-
-# Copy output back to client
-if USE_REDIS:
-    for outputFileName in outputFiles:
-        fullPath = os.path.expanduser(WORKING_DIR) + outputFileName
-        experiment_common.putDataInRedis(fullPath, redisHost=SRC_HOST)
-        sys.stderr.write("Finished adding data from file %s to redis\n" % outputFileName)
-else:
-    for outputFileName in outputFiles:
-        os.system("rsync " + WORKING_DIR + outputFileName + " " + SRC_HOST + ":diamond-src/" + OUTPUT_DEST)
-        sys.stderr.write("Finished syncing output file %s\n" % outputFileName)
+experiment_common.copyFiles("scalability", "apps/benchmarks/build")
+experiment_common.runProcesses(getCommandFunc, NUM_CLIENTS)
+experiment_common.processOutput(processOutputFunc)
