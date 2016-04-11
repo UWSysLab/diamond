80e0385a21ab05812f9bf742453043aecb9ddf70
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-11 11:56:11 -0700

    DOCC container scripts combined into one

---
+++ scripts/gce/docker/run_docc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import argparse
+import experiment_common
+import os
+
+NUM_KEYS = "100"
+NUM_SECONDS = "60"
+OUTPUT_DEST = "scripts/experiments/docc/"
+NUM_CLIENTS = 300
+USE_REDIS = True
+
+parser = argparse.ArgumentParser(description='Run DOCC client.')
+parser.add_argument('optype', choices=['docc', 'baseline'], help='choose between DOCC increment or read/write')
+args = parser.parse_args()
+
+def getCommandFunc(workingDir, configFile, keyFile):
+    cmd = workingDir + "/docc --config " + workingDir + configFile + " --keys " + workingDir + keyFile + " --numkeys " + NUM_KEYS + " --time " + NUM_SECONDS
+    if args.optype == 'docc':
+        cmd = cmd + " --increment"
+    return cmd
+
+def processOutputFunc(outputFile):
+    if USE_REDIS:
+        experiment_common.putDataInRedis(outputFile)
+    else:
+        experiment_common.copyToSrcHost(outputFile, OUTPUT_DEST)
+
+experiment_common.copyFiles("docc", "apps/benchmarks/build")
+experiment_common.runProcesses(getCommandFunc, NUM_CLIENTS, "docc-out")
+experiment_common.processOutput(processOutputFunc)
