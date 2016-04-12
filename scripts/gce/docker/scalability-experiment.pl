50368e5d4c70786c3cb86ce281d1bd728ec44dfd
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-12 09:05:14 -0700

    scalability-experiment.pl resets the client VM and starts/kills redis between every shard/isolation level

--- scripts/gce/docker/scalability-experiment.pl
+++ scripts/gce/docker/scalability-experiment.pl
@@ -19,35 +19,55 @@ my $gceOutputDir = "scalability";
 my $log = "scalability-log.txt";
 system("rm -f $log; touch $log");
 
-my $checkResult = system("./sanity-checks.pl $image $user $script $outputDir $gceOutputDir $GCE_IP $USE_REDIS >> $log 2>&1");
-if ($checkResult != 0) {
-    die("Error in sanity checks: see log file for details");
-}
-
 # run experiment
 my @instanceNums = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 my @shardNums = (1, 2, 3, 4, 5);
 my @isolations = ("linearizable", "snapshot", "eventual");
 
-my $kill_command = "cd diamond-src/scripts; ./manage-servers.py kill ../platform/test/gce";
-system("ssh $GCE_IP '$kill_command'");
+my $resetClientCmd = "gcloud compute instances reset diamond-client-a1o6 --zone us-central1-c >> $log 2>&1; sleep 10";
+my $killServersCmd = "ssh $GCE_IP 'cd diamond-src/scripts; ./manage-servers.py kill ../platform/test/gce' >> $log 2>&1";
+my $startRedisCmd = "ssh -f $GCE_IP 'nohup redis-3.0.7/src/redis-server &' >> $log 2>&1";
+my $killRedisCmd = "ssh $GCE_IP 'pkill redis-server'";
+
+# reset client VM and make sure no Diamond servers are running
+system("$resetClientCmd");
+system("$killServersCmd");
+
+# do sanity checks
+system("$startRedisCmd");
+my $checkResult = system("./sanity-checks.pl $image $user $script $outputDir $gceOutputDir $GCE_IP $USE_REDIS >> $log 2>&1");
+if ($checkResult != 0) {
+    die("Error in sanity checks: see log file for details");
+}
+system("$killRedisCmd");
 
 for my $isolation (@isolations) {
     for my $shards (@shardNums) {
-        system("ssh -t $GCE_IP 'cd diamond-src/scripts; ./manage-servers.py start ../platform/test/gce --keys experiments/keys.txt --numkeys 100000 --shards $shards'");
+        logPrint($log, "Running with isolation level $isolation and $shards shards\n");
+
+        # reset client VM, start redis, and start Diamond servers
+        system("$resetClientCmd");
+        system("$startRedisCmd");
+        system("ssh -t $GCE_IP 'cd diamond-src/scripts; ./manage-servers.py start ../platform/test/gce --keys experiments/keys.txt --numkeys 100000 --shards $shards' >> $log 2>&1");
 
         my $outFile = "$outputDir/$isolation.$shards.txt";
         open(OUTFILE, "> $outFile");
         print(OUTFILE "clients\tthroughput\tlatency\tabortrate\tseconds\tinstances\tisolation\tshards\n");
         for my $instances (@instanceNums) {
-            print("Running $instances instances...\n");
+            logPrint($log, "Running $instances instances...\n");
+
+            # clear output location
             if ($USE_REDIS) {
                 system("ssh $GCE_IP 'redis-3.0.7/src/redis-cli flushdb' >> $log 2>&1");
             }
             else {
                 system("ssh $GCE_IP 'rm diamond-src/scripts/experiments/$gceOutputDir/*' >> $log 2>&1");
             }
+
+            # run Kubernetes instances
             system("./run-kubernetes-job.pl scalability $image $user $instances $script $isolation >> $log 2>&1");
+
+            # parse output
             my $clientCmd = "ls diamond-src/scripts/experiments/scalability | wc | awk \"{ print \\\$1 }\"";
             my $resultCmd = "cd diamond-src/scripts/experiments; ./parse-scalability.py -d $gceOutputDir";
             if ($USE_REDIS) {
@@ -82,6 +102,15 @@ for my $isolation (@isolations) {
         my $time = time();
         system("cp $outFile $outFile.$time");
 
-        system("ssh $GCE_IP '$kill_command'");
+        # kill redis and Diamond servers
+        system("$killRedisCmd");
+        system("$killServersCmd");
     }
+}
+
+sub logPrint {
+    my ($log, $str) = @_;
+    chomp($str);
+    system("echo $str | tee -a $log");
+    return;
 }
