c4e5dcc50f3f9847ff6cdc664e16260222c6365d
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-22 12:10:16 -0700

    Updated baseline experiment graphing script

--- scripts/experiments/plot-baseline.pl
+++ scripts/experiments/plot-baseline.pl
@@ -8,65 +8,77 @@ die $usage unless @ARGV == 1;
 
 my $dir = shift;
 
-my @systems = ("diamond", "baseline");
-my @modes = ("local", "georeplicated");
+my @modes = ("local");
+my @zipfNums = (0.3, 0.8);
 
 my @tempFiles;
 
 for my $mode (@modes) {
-    my $outFile = "temp-$mode.txt";
-    push(@tempFiles, $outFile);
-    open(OUT_FILE, "> $outFile");
-    my $line = 0;
-    for my $system (@systems) {
-        open(IN_FILE, "$dir/$system.$mode.txt");
-        my $maxThroughput = 0;
-        while(<IN_FILE>) {
-            if ($_ =~ /^\d+\s+([\d\.]+)\s+[\d\.]+\s+[\d\.]+\s+[\d\.]+\s+\d+$/) {
-                my $throughput = $1;
-                if ($throughput > $maxThroughput) {
-                    $maxThroughput = $throughput;
-                }
-            }
-        }
-        close(IN_FILE);
-        print(OUT_FILE "$system $maxThroughput\n");
+    for my $zipf (@zipfNums) {
+        my $linearizable = getMaxThroughput("$dir/diamond.$mode.linearizable.$zipf.txt");
+        my $snapshot = getMaxThroughput("$dir/diamond.$mode.snapshot.$zipf.txt");
+        my $eventual = getMaxThroughput("$dir/diamond.$mode.eventual.$zipf.txt");
+        my $baseline = getMaxThroughput("$dir/baseline.$mode.$zipf.txt");
+
+        my $outFile = "temp-$mode-$zipf.txt";
+        push(@tempFiles, $outFile);
+        open(OUT_FILE, "> $outFile");
+        print(OUT_FILE "linearizable $linearizable\n");
+        print(OUT_FILE "snapshot $snapshot\n");
+        print(OUT_FILE "eventual $eventual\n");
+        print(OUT_FILE "baseline $baseline\n");
+        close(OUT_FILE);
     }
-    close(OUT_FILE);
 }
 
 my $script = "temp-gnuplot-script.txt";
 push(@tempFiles, $script);
 
-my $georeplicated = "temp-georeplicated.txt";
-my $local = "temp-local.txt";
+my $local3 = "temp-local-0.3.txt";
+my $local8 = "temp-local-0.8.txt";
 
 open(SCRIPT, "> $script");
 print(SCRIPT "set terminal pdfcairo\n");
-print(SCRIPT "set output \"baseline-georeplicated.pdf\"\n");
+print(SCRIPT "set output \"baseline-local-0.3.pdf\"\n");
 print(SCRIPT "set ylabel \"Throughput (txn/s)\"\n");
 print(SCRIPT "set yrange [0:*]\n");
 print(SCRIPT "set boxwidth 0.5\n");
 print(SCRIPT "set style fill solid\n");
 print(SCRIPT "unset key\n");
-print(SCRIPT "plot \"$georeplicated\" using (column(0)):2:xtic(1) with boxes\n");
+print(SCRIPT "plot \"$local3\" using (column(0)):2:xtic(1) with boxes\n");
 close(SCRIPT);
 
 system("gnuplot $script");
 
 open(SCRIPT, "> $script");
 print(SCRIPT "set terminal pdfcairo\n");
-print(SCRIPT "set output \"baseline-local.pdf\"\n");
+print(SCRIPT "set output \"baseline-local-0.8.pdf\"\n");
 print(SCRIPT "set ylabel \"Throughput (txn/s)\"\n");
 print(SCRIPT "set yrange [0:*]\n");
 print(SCRIPT "set boxwidth 0.5\n");
 print(SCRIPT "set style fill solid\n");
 print(SCRIPT "unset key\n");
-print(SCRIPT "plot \"$local\" using (column(0)):2:xtic(1) with boxes\n");
+print(SCRIPT "plot \"$local8\" using (column(0)):2:xtic(1) with boxes\n");
 close(SCRIPT);
 
 system("gnuplot $script");
 
-for my $file (@tempFiles) {
-    system("rm $file");
+#for my $file (@tempFiles) {
+#    system("rm $file");
+#}
+
+sub getMaxThroughput {
+    my $file = shift;
+    open(IN_FILE, $file);
+    my $maxThroughput = 0;
+    while(<IN_FILE>) {
+        if ($_ =~ /^\d+\s+([\d\.]+)\s+[\d\.]+\s+[\d\.]+\s+[\d\.]+\s+\d+$/) {
+            my $throughput = $1;
+            if ($throughput > $maxThroughput) {
+                $maxThroughput = $throughput;
+            }
+        }
+    }
+    close(IN_FILE);
+    return $maxThroughput;
 }
