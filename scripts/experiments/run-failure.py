d1c2bef42cf899a0b34c47424d4c777a1d4bd58d
Niel Lebeck <niel.lebeck@gmail.com>
2016-04-24 19:11:26 +0000

    Updated run-failure.py to run experiment on GCE

--- scripts/experiments/run-failure.py
+++ scripts/experiments/run-failure.py
@@ -5,17 +5,17 @@ import subprocess
 import time
 
 
-os.system("cd ..; ./manage-servers.py start ../platform/test/niel --shards 1 --batch 1")
+os.system("cd ..; ./manage-servers.py start ../platform/test/gce --shards 1 --batch 1 --frontends 1")
 
-game1 = subprocess.Popen("../../apps/benchmarks/build/game --name player1 --config ../../platform/test/niel.frontend0 --keyprefix test > failure-output-1.txt 2>&1", shell=True)
-game2 = subprocess.Popen("../../apps/benchmarks/build/game --name player2 --config ../../platform/test/niel.frontend0 --keyprefix test > failure-output-2.txt 2>&1", shell=True)
+game1 = subprocess.Popen("../../apps/benchmarks/build/game --name player1 --config ../../platform/test/gce.frontend0 --keyprefix test > failure/failure-output-1.txt 2>&1", shell=True)
+game2 = subprocess.Popen("../../apps/benchmarks/build/game --name player2 --config ../../platform/test/gce.frontend0 --keyprefix test > failure/failure-output-2.txt 2>&1", shell=True)
 
-time.sleep(3)
+time.sleep(15)
 
 print("Killing backend leader...")
-os.system("pkill -f 'diamond0.config -i 0'")
+os.system("ssh diamond-backend-central-4clv 'pkill -9 -f diamond0.config'")
 
 game1.wait()
 game2.wait()
 
-os.system("cd ..; ./manage-servers.py kill ../platform/test/niel")
+os.system("cd ..; ./manage-servers.py kill ../platform/test/gce")
