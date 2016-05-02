c32b0fc5041c6e21d32b3ac09346b9a6862950a5
Niel Lebeck <niel.lebeck@gmail.com>
2016-05-02 19:41:26 +0000

    Fixed typo in run-server script

--- apps/chat/BaselineChatServer/run-server.pl
+++ apps/chat/BaselineChatServer/run-server.pl
@@ -8,4 +8,4 @@ die $usage unless @ARGV==3;
 
 my ($port, $redisHostname, $redisPort) = @ARGV;
 
-system("java -classpath libs/commons-pool2-2.0.jar:libs/gson-2.3.1.jar:libs/jedis-2.4.2.jar:src Main $port $redisHostname $redisPort");
+system("java -classpath libs/commons-pool2-2.0.jar:libs/gson-2.3.1.jar:libs/jedis-2.4.2.jar:bin Main $port $redisHostname $redisPort");
