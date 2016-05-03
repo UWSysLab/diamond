c09692a7ea24c46bfc6e1b57e0a625989a6061e4
Irene Zhang <iyzhang@charlottetown.cs.washington.edu>
2016-05-03 09:36:53 -0700

    fixing bug in caching

--- platform/store/common/frontend/cacheclient.cc
+++ platform/store/common/frontend/cacheclient.cc
@@ -72,14 +72,17 @@ CacheClient::Get(const uint64_t tid, const string &key, const Timestamp &timesta
 
     Version value;
     // look for it in the cache
-    if (cachingEnabled && timestamp != MAX_TIMESTAMP && cache.Get(key, timestamp, value)) {
-        // Make some decision about the timestamp?
-        cache_lock.unlock();
-        
-	Debug("CACHE HIT %s at ts %lu", key.c_str(), value.GetTimestamp());
-        if (promise != NULL)
-            promise->Reply(REPLY_OK, key, value);
-        return;
+    if (cachingEnabled) {
+	if ((timestamp == MAX_TIMESTAMP &&
+	     cache.Get(key, value)) ||
+	    cache.Get(key, timestamp, value)) {
+	    cache_lock.unlock();
+			
+	    Debug("CACHE HIT %s at ts %lu", key.c_str(), value.GetTimestamp());
+	    if (promise != NULL)
+		promise->Reply(REPLY_OK, key, value);
+	    return;
+	}
     }
 
     cache_lock.unlock();
@@ -88,16 +91,16 @@ CacheClient::Get(const uint64_t tid, const string &key, const Timestamp &timesta
     Promise *pp = (promise != NULL) ? promise : &p;
 
     txnclient->Get(tid, key, timestamp, pp);
-    if (pp->GetReply() == REPLY_OK) {
-        if (cachingEnabled) {
-            Debug("Adding [%s] with ts %lu to the cache", key.c_str(), pp->GetValue(key).GetTimestamp());
-            cache_lock.lock();
-            // Make sure that we've capped the validity range
-            ASSERT(pp->GetValue(key).GetInterval().End() != MAX_TIMESTAMP);
-            cache.Put(key, pp->GetValue(key));
-            cache_lock.unlock();
-        }
-    }
+    // if (pp->GetReply() == REPLY_OK) {
+    //     if (cachingEnabled) {
+    //         Debug("Adding [%s] with ts %lu to the cache", key.c_str(), pp->GetValue(key).GetTimestamp());
+    //         cache_lock.lock();
+    //         // Make sure that we've capped the validity range
+    //         ASSERT(pp->GetValue(key).GetInterval().End() != MAX_TIMESTAMP);
+    //         cache.Put(key, pp->GetValue(key));
+    //         cache_lock.unlock();
+    //     }
+    // }
 }
 
 void
@@ -114,11 +117,11 @@ CacheClient::MultiGet(const uint64_t tid, const vector<string> &keys, const Time
     for (auto &key : keys) {
         // look for it in the cache
         if (cachingEnabled) {
-	    if (timestamp == MAX_TIMESTAMP && cache.Get(key, value)) {
+	    if ((timestamp == MAX_TIMESTAMP &&
+		 cache.Get(key, value)) ||
+		cache.Get(key, timestamp, value)) {
 		keysRead[key] = value;
-	    } else (cache.Get(key, timestamp, value)) {
-            // Make some decision about the timestamp?
-            keysRead[key] = value;  
+	    }
         } else {
             keysToRead.push_back(key);
         }
@@ -139,16 +142,16 @@ CacheClient::MultiGet(const uint64_t tid, const vector<string> &keys, const Time
     if (pp->GetReply() == REPLY_OK){
         map<string, Version> values = pp->GetValues();
 
-        cache_lock.lock();
-        for (auto &value : values) {
-            if (cachingEnabled) {
-                Debug("Adding [%s] with ts %lu to the cache", value.first.c_str(), value.second.GetTimestamp());
-                ASSERT(value.second.GetInterval().End() != MAX_TIMESTAMP);
-                cache.Put(value.first, value.second);
-                keysRead[value.first] = value.second;
-            }
-        }
-        cache_lock.unlock();
+        // cache_lock.lock();
+        // for (auto &value : values) {
+        //     if (cachingEnabled) {
+        //         Debug("Adding [%s] with ts %lu to the cache", value.first.c_str(), value.second.GetTimestamp());
+        //         ASSERT(value.second.GetInterval().End() != MAX_TIMESTAMP);
+        //         cache.Put(value.first, value.second);
+        //         keysRead[value.first] = value.second;
+        //     }
+        // }
+        // cache_lock.unlock();
         pp->Reply(REPLY_OK, keysRead);
     }
 }
